bd85695519753865968c46b403c6f5cd2ce4dca7
clyerica <cly.erica@gmail.com>
2022-08-21 15:11:32 +0800

    Added create recipes and find recipe by ID

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ let app = express();
 
 // !! Enable processing JSON data
 app.use(express.json());
-
 // !! Enable CORS
 app.use(cors());
 
@@ -23,11 +22,35 @@ async function main() {
         res.send("hello world")
     })
 
-    // user signup
+    app.get('/recipes/find/:id', async function(req,res){
+        let id=req.params.id
+        const searchRecipe=await db.collection('recipes').findOne({'_id': ObjectId(`${id}`)})
+        if(searchRecipe){
+            displayRecipe=searchRecipe.toArray()
+            res.status(200)
+            res.send(displayRecipe)
+        }
+        else{
+            res.status(404)
+            res.json({"message":"No record found"})
+        }
+        
+    })
+
+    // user signup  
     app.post('/users', async function (req, res) {
         let email = req.body.email
         let password = req.body.password
-        if ( email.includes('@','.') && password.length>7) {
+        let user = await db.collection('users').findOne({
+            'email': req.body.email
+        })
+        if (user) {
+            res.status(400);
+            res.json({
+                'message': "This email already has an account!"
+            })
+        }
+        else if (email.includes('@', '.') && password.length > 7) {
             let newUser = {
                 "email": req.body.email,
                 "password": req.body.password,
@@ -38,24 +61,24 @@ async function main() {
                 'message': "New user created!"
             })
         }
-        else{
-            message=""
-            if (!email.includes('@','.')){
-                message= message + "Email address is not valid. "
+        else {
+            message = ""
+            if (!email.includes('@', '.')) {
+                message = message + "Email address is not valid. "
             }
-            if (password.length<=7){
-                message= message +"Password must be 8 or more characters."
+            if (password.length <= 7) {
+                message = message + "Password must be 8 or more characters."
             }
             res.status(400);
             res.json({
-                'message':message
+                'message': message
             })
         }
     })
 
     // login
     app.post('/login', async function (req, res) {
-        // attempt to find a doucment with the same password and email given
+        // attempt to find a document with the same password and email given
         let user = await db.collection('users').findOne({
             'email': req.body.email,
             'password': req.body.password
@@ -66,7 +89,7 @@ async function main() {
                 'email': req.body.email,
                 'user_id': user._id
             }, process.env.TOKEN_SECRET, {
-                'expiresIn': '1h'   // m for minutes, h for hours, w for weeks, d for days
+                'expiresIn': '1d'   // m for minutes, h for hours, w for weeks, d for days
             });
             res.json({
                 'accessToken': token
@@ -89,7 +112,7 @@ async function main() {
                 if (err) {
                     return res.sendStatus(403);
                     res.json({
-                        message:"Unauthenticated token"
+                        message: "Unauthenticated token"
                     })
                 }
 
@@ -100,6 +123,27 @@ async function main() {
             res.sendStatus(401);
         }
     };
+
+    //check profile 
+    app.get('/profile', checkIfAuthenticatedJWT, async function (req, res) {
+        res.send(req.user);
+    })
+
+    //add recipe
+    app.post('/recipes/create', checkIfAuthenticatedJWT, async function (req, res) {
+        let result = await db.collection('recipes').insertOne({
+            "title": req.body.title,
+            "ingredients": req.body.ingredients,
+            "course": req.body.course,
+            "cuisine": req.body.cuisine,
+            "diet": req.body.diet,
+            "serves": req.body.serves,
+            "method": req.body.method,
+            "user_id":req.user.user_id
+        })
+        res.status(201); 
+        res.send(result);
+    })
 }
 
 main();
